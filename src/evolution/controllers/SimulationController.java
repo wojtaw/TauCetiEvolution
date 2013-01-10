ee6c01e7501a4972c0bc30cae61f3148b16aab2d
SL <VojtaCiml@gmail.com>
2013-01-11 00:29:50 +0100

    added first simulation

--- src/evolution/controllers/SimulationController.java
+++ src/evolution/controllers/SimulationController.java
@@ -1,11 +1,29 @@
 package evolution.controllers;
 
-import evolution.ocean.Ocean;
+import evolution.life.BacteriaColony;
+import evolution.output.Output;
 
 public class SimulationController {
+	private BacteriaColony firstColony;
 	
 	public SimulationController(){
-	Ocean VastOcean = new Ocean();	
+		firstColony = new BacteriaColony();
+		firstColony.bornBacterias(50);
+		runSimulationTime(101);
+	}
+
+	private void runSimulationTime(int numberOfSeconds) {
+		for (int i = 0; i < numberOfSeconds; i++) {
+			Output.printLog("-NEXT TIME STEP-");
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			firstColony.calculateNextStep();
+		}
+		
 	}
 	
 
