26f2e7aab3d9974e17bb8e2175c0d925a5b84593
SL <VojtaCiml@gmail.com>
2013-01-10 22:48:11 +0100

    ocean structure

--- src/evolution/ocean/Ocean.java
+++ src/evolution/ocean/Ocean.java
@@ -1,5 +1,10 @@
 package evolution.ocean;
 
 public class Ocean {
+	
+	public Ocean(){
+		//here we will create ocean 100x100 fields and fill them with random sunshie
+		
+	}
 
 }
---
+++ src/evolution/ocean/OceanField.java
@@ -0,0 +1,19 @@
+package evolution.ocean;
+
+public class OceanField {
+	
+	private int sunshine;
+	
+	public OceanField(int sunshine){
+		this.sunshine = sunshine;
+	}
+
+	public int getSunshine() {
+		return sunshine;
+	}
+
+	public void setSunshine(int sunshine) {
+		this.sunshine = sunshine;
+	}
+
+}
